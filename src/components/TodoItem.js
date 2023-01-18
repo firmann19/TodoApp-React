c76af8868bf1b1861fb8666e33ac5079ef3910b6
Firman Ramadhan <firman19ramadhan@gmail.com>
2023-01-18 13:37:20 +0700

    update 18/01/2023

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,34 @@
-const TodoItem = ({ todo }) => {
-    return (
-      <li className={`${todo.done ? "checked" : ""}`}> 
-        {todo.title} <span className="close">x</span>
-      </li>
-    );
+const TodoItem = ({ todo, setRefresh }) => {
+  const updateTodo = () => {
+    todo.done = !todo.done;
+
+    fetch("http://localhost:5000/todos/" + todo.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    }).then(() => {
+      console.log("todo updated.");
+      setRefresh(true)
+    });
   };
-  
-  export default TodoItem;
+
+  const deleteTodo = () => {
+    fetch("http://localhost:5000/todos/" + todo.id, {
+      method: "DELETE",
+    }).then(() => {
+      console.log('todo deleted.')
+      setRefresh(true);
+    });
+  };
+
+  return (
+    <li className={`${todo.done ? "checked" : ""}`}>
+      <div onClick={updateTodo}>{todo.title}</div>
+      <span className="close" onClick={deleteTodo}>x</span>
+    </li>
+  );
+};
+
+export default TodoItem;
